70b166e90e3377d72dd2c59ab719a58d0f8e4e5d
Clarisse Damon <clarisse.damon@dailymotion.com>
2025-06-06 15:25:51 +0200

    change router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import './App.css'
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import Content from "./components/Content";
 import {MenuProvider} from "./components/menuContext.tsx";
 
